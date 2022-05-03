8de3b949b088de1b0b164c871ce653b976f57e0f
vvbogdanov87 <vvbogdanov87@gmail.com>
2022-05-02 21:00:58 -0700

    Remove legacy Lang dependency Code formatting

--- kubeIndicator.js
+++ kubeIndicator.js
@@ -1,4 +1,3 @@
-const Lang = imports.lang;
 const St = imports.gi.St;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
@@ -14,86 +13,88 @@ const Me = ExtensionUtils.getCurrentExtension();
 const KubePopupMenuItem = Me.imports.kubePopupMenuItem;
 const Util = imports.misc.util;
 
-const KubeIndicator = GObject.registerClass ({GTypeName: 'KubeIndicator'},
+const KubeIndicator = GObject.registerClass({ GTypeName: 'KubeIndicator' },
     class KubeIndicator extends PanelMenu.Button {
-    _init() {
-        super._init(null, "Kube");
-        this._settings = ExtensionUtils.getSettings();
-        this.kcPath = GLib.get_home_dir() + "/.kube/config";
+        _init() {
+            super._init(null, "Kube");
+            this._settings = ExtensionUtils.getSettings();
+            this.kcPath = GLib.get_home_dir() + "/.kube/config";
 
-        this.setMenu(new PopupMenu.PopupMenu(this.actor, 0.25, St.Side.TOP));
+            this.setMenu(new PopupMenu.PopupMenu(this.actor, 0.25, St.Side.TOP));
 
-        this._setView()
+            this._setView()
 
-        let kcFile = Gio.File.new_for_path(this.kcPath);
-        this._monitor = kcFile.monitor(Gio.FileMonitorFlags.NONE, null);
-        this._monitor.connect('changed', Lang.bind(this, this._update));
+            let kcFile = Gio.File.new_for_path(this.kcPath);
+            this._monitor = kcFile.monitor(Gio.FileMonitorFlags.NONE, null);
+            this._monitor.connect('changed', this._update.bind(this));
 
-        this._bindSettingsChanges();
-    }
-
-    _onChange(m, f, of, eventType) {
-        if (eventType == Gio.FileMonitorEvent.CHANGED){
-            this._update()
+            this._bindSettingsChanges();
         }
-    }
 
-    _update() {
-        this.menu.removeAll()
-        try {
-            let contents = ByteArray.toString(GLib.file_get_contents(this.kcPath)[1]);
-            let re = new RegExp('current-context:\\s(.+)','gm');
-            let match = re.exec(contents);
-            let currentContext = '';
-            if (match != null){
-                currentContext = match[1];
-                if ( this._settings.get_boolean('show-current-context') == true ){
-                    this.label.text = currentContext;
-                }
+        _onChange(m, f, of, eventType) {
+            if (eventType == Gio.FileMonitorEvent.CHANGED) {
+                this._update()
             }
+        }
 
-            re = new RegExp('-\\scontext:(\\n.*){1,4}name:\\s(.*)','gm');
-            match = re.exec(contents);
-            while (match != null) {
-                let curr = false;
-                if (match[2]==currentContext){
-                    curr = true;
+        _update() {
+            this.menu.removeAll()
+            try {
+                let contents = ByteArray.toString(GLib.file_get_contents(this.kcPath)[1]);
+                let re = new RegExp('current-context:\\s(.+)', 'gm');
+                let match = re.exec(contents);
+                let currentContext = '';
+                if (match != null) {
+                    currentContext = match[1];
+                    if (this._settings.get_boolean('show-current-context') == true) {
+                        this.label.text = currentContext;
+                    }
                 }
-                this.menu.addMenuItem(new KubePopupMenuItem.KubePopupMenuItem(match[2],curr));
+
+                re = new RegExp('-\\scontext:(\\n.*){1,4}name:\\s(.*)', 'gm');
                 match = re.exec(contents);
+                while (match != null) {
+                    let curr = false;
+                    if (match[2] == currentContext) {
+                        curr = true;
+                    }
+                    this.menu.addMenuItem(new KubePopupMenuItem.KubePopupMenuItem(match[2], curr));
+                    match = re.exec(contents);
+                }
+
+                // add seperator to popup menu
+                this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+
+                // add link to settings dialog
+                this._menu_settings = new PopupMenu.PopupMenuItem(_("Settings"));
+                this._menu_settings.connect("activate", function () {
+                    Util.spawn(["gnome-shell-extension-prefs", "kube_config@vvbogdanov87.gmail.com"]);
+                });
+                this.menu.addMenuItem(this._menu_settings);
+            } catch (e) {
+                log('gnome-shell-extension-kubeconfig', e);
             }
+        }
 
-            // add seperator to popup menu
-            this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+        _setView() {
+            this.actor.remove_all_children();
+            if (this._settings.get_boolean('show-current-context') == false) {
+                let gicon = Gio.icon_new_for_string(Me.path + '/icons/logo.svg');
+                this.icon = new St.Icon({ gicon: gicon, style_class: 'system-status-icon' });
+                this.actor.add_actor(this.icon);
+            } else {
+                this.label = new St.Label({
+                    text: _("kubectl"),
+                    y_align: Clutter.ActorAlign.CENTER
+                });
+                this.actor.add_actor(this.label);
+            }
+            this._update();
+        }
 
-            // add link to settings dialog
-            this._menu_settings = new PopupMenu.PopupMenuItem(_("Settings"));
-            this._menu_settings.connect("activate", function(){
-                Util.spawn(["gnome-shell-extension-prefs", "kube_config@vvbogdanov87.gmail.com"]);
+        _bindSettingsChanges() {
+            this._settings.connect('changed::show-current-context', () => {
+                this._setView();
             });
-            this.menu.addMenuItem(this._menu_settings);
-        } catch (e) {
-            log('gnome-shell-extension-kubeconfig',e);
         }
-    }
-
-    _setView() {
-        this.actor.remove_all_children();
-        if ( this._settings.get_boolean('show-current-context') == false ){
-            let gicon = Gio.icon_new_for_string( Me.path + '/icons/logo.svg' );
-            this.icon = new St.Icon({ gicon: gicon, style_class: 'system-status-icon' });
-            this.actor.add_actor(this.icon);
-        } else {
-            this.label = new St.Label({ text: _("kubectl"),
-                y_align: Clutter.ActorAlign.CENTER });
-            this.actor.add_actor(this.label);
-        }
-        this._update();
-    }
-
-    _bindSettingsChanges() {
-        this._settings.connect('changed::show-current-context',Lang.bind(this, function() {
-            this._setView();
-        }));
-    }
-});
+    });
--- kubePopupMenuItem.js
+++ kubePopupMenuItem.js
@@ -1,4 +1,3 @@
-const Lang = imports.lang;
 const St = imports.gi.St;
 const PopupMenu = imports.ui.popupMenu;
 const GLib = imports.gi.GLib;
@@ -8,41 +7,41 @@ const byteArray = imports.byteArray;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Gio = imports.gi.Gio;
 
-const KubePopupMenuItem = GObject.registerClass ({GTypeName: 'KubePopupMenuItem'},
+const KubePopupMenuItem = GObject.registerClass({ GTypeName: 'KubePopupMenuItem' },
     class extends PopupMenu.PopupBaseMenuItem {
-        _init(text,selected, params) {
-        super._init(params);
-        this.text = text;
-        this.selected = selected;
-        this.box = new St.BoxLayout({ style_class: 'popup-combobox-item' });
+        _init(text, selected, params) {
+            super._init(params);
+            this.text = text;
+            this.selected = selected;
+            this.box = new St.BoxLayout({ style_class: 'popup-combobox-item' });
 
-        if( this.selected == true ) {
-            let gicon = Gio.icon_new_for_string( Me.path + '/icons/ball.svg' );
-            this.icon = new St.Icon({ gicon: gicon, style_class: 'system-status-icon', icon_size: 16 });
-            this.box.add(this.icon);
-            this.text = " " + this.text
-        }
-
-        this.label = new St.Label({ text: this.text });
-        this.box.add(this.label);
-
-        this.actor.add_child(this.box);
-
-        this.connect("activate", Lang.bind(this, function(){
-            const path = GLib.build_filenamev([GLib.get_home_dir(), "/.kube/config"]);
-            try {
-                const file = Gio.File.new_for_path(path);
-                const [_, buffer] = file.load_contents(null);
-                let contents = byteArray.toString(buffer);
-
-                const re = new RegExp('current-context:\\s(.+)','gm');
-                contents = contents.replace(re, `current-context: ${this.text.trim()}`);
-
-                // Using `Gio.FileCreateFlags.NONE` maintains file permissions
-                file.replace_contents(contents, null, false, Gio.FileCreateFlags.NONE, null);
-            } catch (e) {
-                log('gnome-shell-extension-kubeconfig',e);
+            if (this.selected == true) {
+                let gicon = Gio.icon_new_for_string(Me.path + '/icons/ball.svg');
+                this.icon = new St.Icon({ gicon: gicon, style_class: 'system-status-icon', icon_size: 16 });
+                this.box.add(this.icon);
+                this.text = " " + this.text
             }
-        }));
-    }
-});
+
+            this.label = new St.Label({ text: this.text });
+            this.box.add(this.label);
+
+            this.actor.add_child(this.box);
+
+            this.connect("activate", () => {
+                const path = GLib.build_filenamev([GLib.get_home_dir(), "/.kube/config"]);
+                try {
+                    const file = Gio.File.new_for_path(path);
+                    const [_, buffer] = file.load_contents(null);
+                    let contents = byteArray.toString(buffer);
+
+                    const re = new RegExp('current-context:\\s(.+)', 'gm');
+                    contents = contents.replace(re, `current-context: ${this.text.trim()}`);
+
+                    // Using `Gio.FileCreateFlags.NONE` maintains file permissions
+                    file.replace_contents(contents, null, false, Gio.FileCreateFlags.NONE, null);
+                } catch (e) {
+                    log('gnome-shell-extension-kubeconfig', e);
+                }
+            });
+        }
+    });
