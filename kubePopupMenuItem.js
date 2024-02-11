21cd14639e6057a99ecee1821e517e9a42231d1f
c84c <616846+c84c@users.noreply.github.com>
2024-02-11 20:24:26 +0100

    refactor: UI/UX

--- kubePopupMenuItem.js
+++ kubePopupMenuItem.js
@@ -10,28 +10,20 @@ export const KubePopupMenuItem = GObject.registerClass(
     {
         GTypeName: 'KubePopupMenuItem'
     },
-    class extends PopupMenu.PopupBaseMenuItem {
+    class extends PopupMenu.PopupMenuItem {
         constructor(extensionObject, text, selected, params) {
-            super(params);
+            super(text.trim(), params);
             this._extensionObject = extensionObject;
-            this.text = text;
-            this.selected = selected;
-            this.box = new St.BoxLayout({ style_class: 'popup-combobox-item' });
-
-            if (this.selected === true) {
-                let gicon = Gio.icon_new_for_string(this._extensionObject.path + '/icons/ball.svg');
-                this.icon = new St.Icon({ gicon: gicon, style_class: 'system-status-icon', icon_size: 16 });
-                this.box.add_child(this.icon);
-                this.text = " " + this.text;
+            // current context ornament
+            if (selected === true) {
+                this.setOrnament(PopupMenu.Ornament.DOT);
+                //this.setOrnament(PopupMenu.Ornament.CHECK);
+            } else {
+                this.setOrnament(PopupMenu.Ornament.NONE);
             }
-
-            this.label = new St.Label({ text: this.text });
-            this.box.add_child(this.label);
-
-            this.add_child(this.box);
-
-            this.connect("activate", () => {
-                KubectlConfig.useContext(this.text.trim());
+            // connect signal
+            this.connect("activate", (_item, _event) => {
+                Kubectl.useContext(this.label.get_text());
             });
         }
     });
